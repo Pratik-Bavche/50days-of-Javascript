28081725a0158890cec106c665226cc34b209222
Pratik <pratikbavche2005@gmail.com>
2026-01-28 15:37:32 +0530

    using reduce

--- CountOccurancesOfChar.js
+++ CountOccurancesOfChar.js
@@ -3,14 +3,25 @@ const countChar=(str,ch)=>{
     str=str.toLowerCase();
     ch=ch.toLowerCase();
     let c=0;
-    for(i=0;i<str.length;i++)
-    {
-        if(str.charAt(i)==ch) c++;
-    }
+
+
+    //Using Reduce Method
+    str=str.split("")
+
+    str.reduce((prev,curr)=>{
+        if(ch===curr) c++;
+    })
+
+    //using LOOP
+
+    // for(i=0;i<str.length;i++)
+    // {
+    //     if(str.charAt(i)==ch) c++;
+    // }
     return c;
 }
 
 
 
 
-console.log(countChar("PraaaaaaAatik",'a'))
+console.log(countChar("PraaaaaaaaAatik",'a'))
