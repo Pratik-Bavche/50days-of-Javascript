c41a84c228cfd46df3d791b2ee41e4d1afa0bbae
Pratik <pratikbavche2005@gmail.com>
2026-01-31 10:17:51 +0530

    Day 6 done

--- PalindromeString.js
+++ PalindromeString.js
@@ -1,5 +1,5 @@
 const isPalindrome=(str)=>{
-    str=str.toLowerCase().replace(/\W/g,"")
+    str=str.toLowerCase().replace(/\W/g,"") //replace all with space except A-Z,a-z,0-9
 
     let rstr=str.split("").reverse().join('')
 
