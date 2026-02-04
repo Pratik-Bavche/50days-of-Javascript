8bc4b237676fada784e9f8a37ee964c19693eb30
Pratik <pratikbavche2005@gmail.com>
2026-02-04 10:19:42 +0530

    refactor: simplify array equality check using Array.every method

--- ArrayEqual.js
+++ ArrayEqual.js
@@ -1,8 +1,13 @@
 
 const ArrayEQ=(a1,a2)=>{
     if(a1.length==0 || a2.length==0 || a1.length!=a2.length) return false;
-    for(i=0;i<a1.length;i++) if(a1[i]!=a2[i]) return false;
-    return true;
+
+    //Using Loops
+    // for(i=0;i<a1.length;i++) if(a1[i]!=a2[i]) return false;
+    // return true;
+
+    //Using Every Method
+    return a1.every((curr,ind)=>curr==a2[ind])
 }
 
 
