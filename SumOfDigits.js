450b109b9b42a3e21e9c52077ff1a280fa891ed2
Pratik <pratikbavche2005@gmail.com>
2026-02-05 10:03:42 +0530

    using map

--- SumOfDigits.js
+++ SumOfDigits.js
@@ -1,7 +1,13 @@
 const sumOfDigits=(arr)=>{
     // console.log(typeof arr)
-    arr=Array.from(String(arr),Number)
-    return arr.reduce((prev,curr)=>curr+=prev,0)
+    
+    // arr=Array.from(String(arr),Number)
+    // return arr.reduce((prev,curr)=>curr+=prev,0)
+
+    //using map
+    sum=0;
+    arr.map((prev)=>sum+=prev,0)
+    return sum;
 }
 
 
