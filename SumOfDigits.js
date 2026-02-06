5390cbecfc53811a60272c89bb42e3bdabbd6e6a
Pratik <pratikbavche2005@gmail.com>
2026-02-06 08:13:43 +0530

    sum of digit update

--- SumOfDigits.js
+++ SumOfDigits.js
@@ -1,7 +1,7 @@
 const sumOfDigits=(arr)=>{
     // console.log(typeof arr)
     
-    // arr=Array.from(String(arr),Number)
+    arr=Array.from(String(arr),Number)
     // return arr.reduce((prev,curr)=>curr+=prev,0)
 
     //using map
