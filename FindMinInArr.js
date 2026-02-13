ccf3e7919c2020bc5c53bb2c6b3a8987d428e42b
Pratik <pratikbavche2005@gmail.com>
2026-02-13 08:17:32 +0530

    using reduce

--- FindMinInArr.js
+++ FindMinInArr.js
@@ -1,14 +1,23 @@
 
 const findMin = (arr) => {
-    let less = Number.MAX_SAFE_INTEGER;
+    // let less = Number.MAX_SAFE_INTEGER;
 
-    arr.map(curr => {
-        if (curr < less) {
-            less = curr;
-        }
-    });
+    // arr.map(curr => {
+    //     if (curr < less) {
+    //         less = curr;
+    //     }
+    // });
 
-    return less;
+    // return less;
+
+    //Using Reduce
+
+    
+    return arr.reduce((prev, curr) => prev < curr ? prev : curr);
+    
+    return arr.reduce((prev, curr) =>  Math.min(prev, curr));
+
+
 };
 
 
