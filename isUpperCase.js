9a3c78502430e852da1a703f2d8efeb2a55b5def
Pratik <pratikbavche2005@gmail.com>
2026-02-15 17:42:33 +0530

    Simple Approach

--- isUpperCase.js
+++ isUpperCase.js
@@ -1,12 +1,16 @@
-const isUpper = (ch) => {
-    const ascii = ch.charCodeAt();
-    return ascii >= 65 && ascii <= 90;
-}
+// const isUpper = (ch) => {
+//     const ascii = ch.charCodeAt();
+//     return ascii >= 65 && ascii <= 90;
+// }
 
-const isLower = (ch) => {
-    const ascii = ch.charCodeAt();
-    return ascii >= 97 && ascii <= 122;
-}
+// const isLower = (ch) => {
+//     const ascii = ch.charCodeAt();
+//     return ascii >= 97 && ascii <= 122;
+// }
+
+//Simple Approach
+const isUpper = (ch) => ch >= 'A' && ch <= 'Z';
+const isLower = (ch) => ch >= 'a' && ch <= 'z';
 
 console.log(isUpper("a"))  // true
 console.log(isLower("A"))  // true
