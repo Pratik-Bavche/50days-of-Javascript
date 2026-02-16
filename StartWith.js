b549bb87b98e300ef919447370bc8070bc8d2959
Pratik <pratikbavche2005@gmail.com>
2026-02-16 07:34:38 +0530

    refactor: replace startsWith with slice for case-insensitive check

--- StartWith.js
+++ StartWith.js
@@ -1,7 +1,10 @@
 const startWith=(str,check)=>{
-   return str.toLowerCase().startsWith(check.toLowerCase())
+//    return str.toLowerCase().startsWith(check.toLowerCase())
+
+    // Using Slice
+    return str.toLowerCase().slice(0,check.length)===check.toLowerCase()
 }
 
 
 
-console.log(startWith("hello world","Hello"))
+console.log(startWith("Hello world","hello"))
