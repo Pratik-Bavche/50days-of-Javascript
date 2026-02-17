eb02eb2aa358ba8dc937b849832419e06e418049
Pratik <pratikbavche2005@gmail.com>
2026-02-17 08:01:57 +0530

    refactor: update revStr function to use loops instead of split for string reversal

--- ReverseString.js
+++ ReverseString.js
@@ -1,8 +1,17 @@
 
 const revStr=(str)=>{
-    return str.split("").reverse().join("");
+    //using split
+    // return str.split("").reverse().join("");
+
+    //Using loops
+    let s="";
+    for(i=str.length-1;i>=0;i--)
+    {
+        s+=str.charAt(i);
+    }
+    return s;
 }
 
-
+//String in JS are immutable
 
 console.log(revStr("Abc"))
