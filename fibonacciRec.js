1fd68b3229eff0ff97a4061032109772d79d057d
Pratik <pratikbavche2005@gmail.com>
2026-02-23 07:59:46 +0530

    priniting

--- fibonacciRec.js
+++ fibonacciRec.js
@@ -1,7 +1,8 @@
 const fibonacci = (no) => {
     if(no <= 1) return no;
 
-    return fibonacci(no - 1) + fibonacci(no - 2);
+     console.log(s=fibonacci(no - 1) + fibonacci(no - 2));
+     return s;
 }
 
 console.log(fibonacci(5));  
