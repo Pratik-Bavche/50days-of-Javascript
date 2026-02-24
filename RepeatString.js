14c0df2ab30a37884a6c59ba442be0f20b2539b6
Pratik <pratikbavche2005@gmail.com>
2026-02-24 08:09:32 +0530

    using inbuikt

--- RepeatString.js
+++ RepeatString.js
@@ -1,11 +1,16 @@
 
 const repeatString=(str,c)=>{
-    let a=""
-    for(i=0;i<c;i++)
-    {
-        a+=str;
-    }
-    return a;
+    // let a=""
+    // for(i=0;i<c;i++)
+    // {
+    //     a+=str;
+    // }
+    // return a;
+
+
+    //USING INBUILT
+
+    return str.repeat(c);
 }
 
 
