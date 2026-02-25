e4d1d977b0eda30c9d646282ccb3fbc69671285a
Pratik <pratikbavche2005@gmail.com>
2026-02-25 08:41:48 +0530

    ternary op

--- TruncateString.js
+++ TruncateString.js
@@ -1,10 +1,12 @@
 
 const truncateString=(str,c)=>{
-    if(c<=0) return str;
-    if(str.length>c)
-    {
-        return str.substring(0,7)+"...";
-    }
+    // if(c<=0) return str;
+    // if(str.length>c)
+    // {
+    //     return str.substring(0,7)+"...";
+    // }
+
+    return (c<=0)?str:str.substring(0,7)+"...";
 }
 
 
