9d076d9f62c93ecf1c4744df9ddcfff9ba75725b
Pratik <pratikbavche2005@gmail.com>
2026-03-01 09:05:05 +0530

    updated hexRandom generator

--- RandomHexColor.js
+++ RandomHexColor.js
@@ -1,8 +1,9 @@
 
 const randomHex=()=>{
-    return Math.random().toString(16).substring(2,8);
+    return `#${Math.random().toString(16).substring(2,8).padEnd(6,0)}`;
 }
 
+//padEnd used to add extra letter if there are ot 6 letters in string then a the end add 0's
 
 
 console.log(randomHex())
